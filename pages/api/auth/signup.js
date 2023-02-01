84210cfa543f434e293ff90849759a9bd071842e
sahrilputra <iam.sahrilputra@gmail.com>
2023-02-02 02:30:02 +0800

    api validation signup

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,4 +1,5 @@
 import nc from "next-connect";
+import {validateEmail} from "../../../utils/validation"
 import db from "../../../utils/db"
 const handler = nc();
 handler.post(async(req, res)=>{
@@ -8,27 +9,12 @@ handler.post(async(req, res)=>{
         if (!name || !email || !password){
             return res.status(400).json({ message: "Pliss fill in all fields."});
         }
-        console.log(req.body);
+        if(!validateEmail(email)){
+            return res.status(400).json({ message: "invalid Email."});
+        }
+
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 })
 export default handler
-
-// import nc from "next-connect";
-// import db from "../../../utils/db"
-// const handler = nc();
-
-// handler.post(async (req, res) => {
-//   try {
-//     await db.connectDb();
-//     const { name, email, password } = req.body;
-//     if (!name || !email || !password) {
-//       return res.status(400).json({ message: "Please fill in all fields." });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// export default handler;
