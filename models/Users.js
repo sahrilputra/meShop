851962c1fb082679472749ae9a2134360ea2392b
sahrilputra <iam.sahrilputra@gmail.com>
2023-02-02 16:40:26 +0800

    user signup schema modeling

--- models/Users.js
+++ models/Users.js
@@ -1,76 +1,75 @@
 import mongoose from "mongoose";
+const {
+    ObjectId
+} = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-            name: {
-                type: String,
-                required: "please enter your full name",
-
-            },
-            email: {
-                type: String,
-                required: "please enter your email address",
-                trim: true,
-                uniqe: true,
-            },
-            passowrd: {
-                type: String,
-                required: "please enter your password"
-            },
-            role: {
-                type: String,
-                default: "user",
-            },
-            image: {
-                type: String,
-                default: "https://w7.pngwing.com/pngs/419/473/png-transparent-computer-icons-user-profile-login-user-heroes-sphere-black-thumbnail.png"
-            },
-            emailVerified: {
-                type: Boolean,
-                default: false,
-            },
-            defaultPaymentMethod: {
-                type: String,
-                default: ''
-            },
-            address: [{
-                firstName: {
-                    type: String,
-                },
-                lastName: {
-                    type: String,
-                },
-                phoneNumber: {
-                    type: String,
-                },
-                address1: {
-                    type: String,
-                },
-                address2: {
-                    type: String,
-                },
-                city: {
-                    type: String,
-                },
-                zipCode: {
-                    type: String,
-                },
-                state: {
-                    type: String,
-                },
-                country: {
-                    type: String,
-                },
-                activity: {
-                    type: Boolean,
-                    default: false,
-                },
-            }]
+    name: {
+        type: String,
+        required: "Please enter your full name.",
+    },
+    email: {
+        type: String,
+        required: "Please enter your email address.",
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: '"Please enter a password.',
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    image: {
+        type: String,
+        default: "https://res.cloudinary.com/dmhcnhtng/image/upload/v1664642478/992490_b0iqzq.png",
+    },
+    emailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    defaultPaymentMethod: {
+        type: String,
+        default: "",
+    },
+    address: [{
+        firstName: {
+            type: String,
         },
-    {
-        timestamps: true,
-    }
-);
+        lastName: {
+            type: String,
+        },
+        phoneNumber: {
+            type: String,
+        },
+        address1: {
+            type: String,
+        },
+        address2: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        zipCode: {
+            type: String,
+        },
+        state: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+        active: {
+            type: Boolean,
+            default: false,
+        },
+    }, ],
+}, {
+    timestamps: true,
+});
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-const User = mongoose.model.User || mongoose.model('User', userSchema);
-
-export default User
+export default User;
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,20 +1,67 @@
 import nc from "next-connect";
-import {validateEmail} from "../../../utils/validation"
+import {
+    validateEmail
+} from "../../../utils/validation"
+import {
+    createActivationToken
+} from "../../../utils/tokens"
 import db from "../../../utils/db"
+import User from "../../../models/Users"
+import bcrypt from "bcrypt";
+
 const handler = nc();
-handler.post(async(req, res)=>{
+handler.post(async (req, res) => {
     try {
         await db.connectDb();
-        const {name, email, password} = req.body;
-        if (!name || !email || !password){
-            return res.status(400).json({ message: "Pliss fill in all fields."});
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                message: "Pliss fill in all fields."
+            });
         }
-        if(!validateEmail(email)){
-            return res.status(400).json({ message: "invalid Email."});
+        if (!validateEmail(email)) {
+            return res.status(400).json({
+                message: "invalid Email."
+            });
         }
 
+        const user = await User.findOne({
+            email
+        });
+        if (user) {
+            return res.status(400).json({
+                message: "This Email already taken."
+            });
+        }
+        if (password.length < 6) {
+            return res
+                .status(400)
+                .json({
+                    message: "Password must be atleast 6 characters."
+                });
+        }
+        const cryptedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            name,
+            email,
+            password: cryptedPassword
+        });
+        const addedUser = await newUser.save();
+
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+        });
+        console.log(activation_token);
+        res.send(activation_token);
+
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({
+            message: error.message
+        });
     }
 })
 export default handler
