4054149f071985ba25241a601ec51b6335061b58
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-21 09:20:50 +0700

    category content listing

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,11 +1,12 @@
 import styles from './styles.module.scss'
 import MainSwipper from './swipper'
 import OffersComponent from './offers'
+import Menu from './menu'
 export const Main = () => {
   return (
     <div className={styles.main}>
       <div className={styles.header}>header</div>
-      <div className={styles.menu}>menu</div>
+      <Menu />
       <MainSwipper />
       <OffersComponent />
       <div className={styles.user}>users</div>
--- data/home.js
+++ data/home.js
@@ -35,3 +35,330 @@ export const offersAarray = [{
         discount: "5",
     },
 ];
+export const menuArray = [{
+        name: "Women's Fashion",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Men's Fashion",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Electronics",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Jewlery & Watches",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Home, Pet & Appliances",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Beauty, Health & Hair",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+
+    {
+        name: "Shoes,Sneakers,Heels",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+
+    {
+        name: "Accessories",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+
+    {
+        name: "Sports & Entertainment ",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Kids & Babies",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Movies & Television",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Gaming, Video Games",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Phones",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Toys & Hobbies",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Gifts & Crafts",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Machinery",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+    {
+        name: "Security, Safety",
+        link: "",
+        subMenu: [{
+            name: "",
+            link: "",
+        }, ],
+        brands: [],
+        images: [],
+    },
+];
+
+export const women_dresses = [{
+        image: "https://img.ltwebstatic.com/images3_pi/2022/01/10/16417808659209e55eaeb6216fe1b0ab7569f434e9_thumbnail_600x.webp",
+        price: "79.58",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/04/25/16508565378f388bf7e888c2bc94273e9953d22100_thumbnail_600x.webp",
+        price: "",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/03/24/164810755649b8cabd855efc56aa7a0a6b422962fe_thumbnail_600x.webp",
+        price: "",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/04/11/164964281857e58b1fa7a38f9fff092384f23801f9_thumbnail_600x.webp",
+        price: "",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2021/10/15/16342654110372b23c51d76ca165537318bd0828f1_thumbnail_600x.webp",
+        price: "",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/02/21/1645432934d3afbf3ceebe3cbeb48019a7e4098f0f_thumbnail_600x.webp",
+        price: "",
+    },
+];
+export const women_shoes = [{
+        image: "https://img.ltwebstatic.com/images3_pi/2021/04/24/16192428449d0a2f9df3090b84cded584abe64c71e_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/06/14/165517428253e93bd6a795d9a97c21810ef1beb61c_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2021/07/28/1627454227e78cf5c9d505d6c45a1c11e6c9b83625_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/05/13/1652449751b1c4055651a96b57ae161885def48c15_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2021/08/13/1628869751ad4c4dbe554b72f65af9d5c532528f6c_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/04/27/1651047458260c8e9c705b77227c9acdba9104c367_thumbnail_600x.webp",
+    },
+];
+export const women_accessories = [{
+        image: "https://img.ltwebstatic.com/images3_pi/2022/05/12/1652333770b01690f4b990751e070cc10e4682b39d_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2021/10/18/1634549753f940286e6a06890c2a30302bbdbbf665_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/03/15/1647310895dcbf38181ac1254338a636f64c94384b_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/05/13/1652449751b1c4055651a96b57ae161885def48c15_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/06/07/1654567858a331ceb5ce709ed8e53bc50b390d19f9_thumbnail_600x.webp",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/06/20/16557062656c0432e06a65323afab13e64550c45d6_thumbnail_600x.webp",
+    },
+];
+
+export const women_swiper = [{
+        name: "HARRY POTTER | SHEIN Checkered & Snake Pattern Sweater Vest Without Blouse",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/06/21/1655799794344eb21cae76cf679de869f6b6e41229_thumbnail_600x.webp",
+        price: "103",
+    },
+    {
+        name: "Blazer manches cape à bouton",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/13/1663060175905a8d4a3995fd5bc4c4827114c4b070_thumbnail_900x.webp",
+        price: "58",
+    },
+    {
+        name: "SHEIN X Zhihan Wang Sweat-shirt dessin animé et slogan",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/29/1664415954a61747c30c7e9576fd4374354dad4b54_thumbnail_900x.webp",
+        price: "12.99",
+    },
+    {
+        name: "ROMWE Grunge Punk Sweat-shirt à capuche à imprimé lettre et graphique à cordon",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/14/166312290414845f283801744986928b2d3dd8d2a9_thumbnail_405x552.webp",
+        price: "396",
+    },
+    {
+        name: "15pcs Gemstone & Rhinestone Decor Ring",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/08/24/16613054802b93849c39b4b566941f1b5ebe9a1626_thumbnail_600x.webp",
+        price: "5.31",
+    },
+    {
+        name: "5pcs Halloween Pumpkin & Ghost Design Shoe Decoration",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/08/18/166082359241270ee7699993abcaefaad6fdedcfc2_thumbnail_600x.webp",
+        price: "79.25",
+    },
+    {
+        name: "10pcs Solid Hair Band",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/07/11/16575212726da2c7d3a0580bc5cfcfeb153d622924_thumbnail_600x.webp",
+        price: "3.69",
+    },
+    {
+        name: "Rhinestone Decor Bracelet With Ring",
+        image: "https://img.ltwebstatic.com/images3_pi/2021/07/30/1627631651e33bb2d80d523abbcc83e3e18a43e76a_thumbnail_600x.webp",
+        price: "12.5",
+    },
+    {
+        name: "Colorful Leaf Print Pocket Front Backpack",
+        image: "https://img.ltwebstatic.com/images3_pi/2022/05/27/16536468000ce423c3ae45c06b70ae48ef8c78ecde_thumbnail_405x552.webp",
+        price: "11.58",
+    },
+];
+
+export const gamingSwiper = [{
+        name: "",
+        image: "https://ae01.alicdn.com/kf/H67f1ee99443a46d2a46b0ab346aae2b6Y.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/H46f7fcad7ab84793a0e69acfc856285d5/desktops-computer-quad-core-gaming-laptops-6GB-Grafiken-computer-desktop-computer-gamer-PC.jpg_Q90.jpg_.webp",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/H3658559222f34898b0d65c78c4eae42fh.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/H6e2ca40dfd9c4701b0005c9925a5008ff.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/S510e42af09264c90bc9f4ea9ca535411u.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/S87eda4e95251435ca81023273f73d8dd1.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/S510e42af09264c90bc9f4ea9ca535411u.jpg",
+        price: "",
+    },
+    {
+        name: "",
+        image: "https://ae01.alicdn.com/kf/S87eda4e95251435ca81023273f73d8dd1.jpg",
+        price: "",
+    },
+];
