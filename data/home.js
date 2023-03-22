86e2638bccb3d1a8262dfba81b195794ff73ef98
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-23 05:44:51 +0700

    home category content

--- data/home.js
+++ data/home.js
@@ -208,6 +208,93 @@ export const menuArray = [{
     },
 ];
 
+export const userSwiperArray = [{
+        image: "https://ae01.alicdn.com/kf/S8bf3306f6f7b4855b147862236a5a12dD/45MM-Transparent-Mod-Kit-Case-for-Apple-Watch-Band-7-45-44MM-Rubber-Sports-Watchband-for.jpg_220x220xz.jpg_.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/Sdb42f04658cd4a9c82018e22e90a78a2X.jpg_220x220.webp_480x480Q90.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/H5914a44106c44f4ca51280c4fcd0a3bfY.jpg_220x220.webp_480x480Q90.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/H5b92665c4bae4160845d4d235586a41b7/Win10-OneGX1-Pro-Mini-Laptop-Gaming-7-inch-Notebook-Computer-Intel-i7-16G-RAM-512G-PICe.jpg_220x220xz.jpg_.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/H01e60132bef1420a849fa84700ebae174.jpg_350x350.jpg_480x480Q90.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/S714e9ef3689b4a31954f2d180d4938c9u.jpg_220x220.webp_480x480Q90.webp",
+        link: "",
+    },
+    {
+        image: "https://ae01.alicdn.com/kf/S31f6384cf5ae41da86f9554615e7e392E.jpg_220x220.webp_480x480Q90.webp",
+        link: "",
+    },
+];
+
+export const flashDealsArray = [{
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/15/1663220709c947ca4ad9aefd9c1f4baf5915653468_thumbnail_600x.webp",
+        price: "91",
+        discount: "11",
+        link: "",
+        sold: "30",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/20/1663668243e6d8f250648c2f34988320c046cba68f_thumbnail_405x552.webp",
+        price: "73",
+        discount: "25",
+        link: "",
+        sold: "61",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/03/25/164818761997d2ccb1de50aed18da9d77502717405_thumbnail_405x552.webp",
+        price: "254",
+        discount: "36",
+        link: "",
+        sold: "12",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/05/13/165244123600952e503ccab89dd363c9b71f9094f8_thumbnail_405x552.webp",
+        price: "1687",
+        discount: "20",
+        link: "",
+        sold: "96",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/06/17/16554568735029a68267cacc48382e23027667093f_thumbnail_405x552.webp",
+        price: "126",
+        discount: "8",
+        link: "",
+        sold: "12",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/08/17/16607290552013caeedd00666350d71774fcd6e95f_thumbnail_405x552.webp",
+        price: "65",
+        discount: "7",
+        link: "",
+        sold: "82",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/02/166210785062f36a63bcecd00693783c4b6cd0c2a2_thumbnail_600x.webp",
+        price: "41",
+        discount: "9",
+        link: "",
+        sold: "52",
+    },
+    {
+        image: "https://img.ltwebstatic.com/images3_pi/2022/09/01/1662028598b9164c30a8410fc9d71b81fc63748d4e_thumbnail_405x552.webp",
+        price: "288",
+        discount: "35",
+        link: "",
+        sold: "49",
+    },
+];
 export const women_dresses = [{
         image: "https://img.ltwebstatic.com/images3_pi/2022/01/10/16417808659209e55eaeb6216fe1b0ab7569f434e9_thumbnail_600x.webp",
         price: "79.58",
@@ -360,90 +447,34 @@ export const gamingSwiper = [{
         price: "",
     },
 ];
-export const userSwiperArray = [{
-        image: "https://ae01.alicdn.com/kf/S8bf3306f6f7b4855b147862236a5a12dD/45MM-Transparent-Mod-Kit-Case-for-Apple-Watch-Band-7-45-44MM-Rubber-Sports-Watchband-for.jpg_220x220xz.jpg_.webp",
-        link: "",
+export const homeImprovSwiper = [{
+        name: "",
+        image: "https://ae01.alicdn.com/kf/H973c7fd8079f4820b28f7e5b8d7e4bb6s.jpg",
+        price: "",
     },
     {
-        image: "https://ae01.alicdn.com/kf/Sdb42f04658cd4a9c82018e22e90a78a2X.jpg_220x220.webp_480x480Q90.webp",
-        link: "",
+        name: "",
+        image: "https://ae01.alicdn.com/kf/S70e0b1be525948e3ae8ca0dbb5672c06Q.jpg",
+        price: "",
     },
     {
-        image: "https://ae01.alicdn.com/kf/H5914a44106c44f4ca51280c4fcd0a3bfY.jpg_220x220.webp_480x480Q90.webp",
-        link: "",
+        name: "",
+        image: "https://ae03.alicdn.com/kf/S0e018b6c00ae409781e815c79d44b57bL.jpg",
+        price: "",
     },
     {
-        image: "https://ae01.alicdn.com/kf/H5b92665c4bae4160845d4d235586a41b7/Win10-OneGX1-Pro-Mini-Laptop-Gaming-7-inch-Notebook-Computer-Intel-i7-16G-RAM-512G-PICe.jpg_220x220xz.jpg_.webp",
-        link: "",
+        name: "",
+        image: "https://ae03.alicdn.com/kf/H3c3a10b673554dc3a2957801aac40174z.jpg",
+        price: "",
     },
     {
-        image: "https://ae01.alicdn.com/kf/H01e60132bef1420a849fa84700ebae174.jpg_350x350.jpg_480x480Q90.webp",
-        link: "",
+        name: "",
+        image: "https://ae03.alicdn.com/kf/Se9ebef1491684e9bbde7f7f97e9fcf78k.jpg",
+        price: "",
     },
     {
-        image: "https://ae01.alicdn.com/kf/S714e9ef3689b4a31954f2d180d4938c9u.jpg_220x220.webp_480x480Q90.webp",
-        link: "",
-    },
-    {
-        image: "https://ae01.alicdn.com/kf/S31f6384cf5ae41da86f9554615e7e392E.jpg_220x220.webp_480x480Q90.webp",
-        link: "",
-    },
-];
-
-export const flashDealsArray = [{
-        image: "https://img.ltwebstatic.com/images3_pi/2022/09/15/1663220709c947ca4ad9aefd9c1f4baf5915653468_thumbnail_600x.webp",
-        price: "91",
-        discount: "11",
-        link: "",
-        sold: "30",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/09/20/1663668243e6d8f250648c2f34988320c046cba68f_thumbnail_405x552.webp",
-        price: "73",
-        discount: "25",
-        link: "",
-        sold: "61",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/03/25/164818761997d2ccb1de50aed18da9d77502717405_thumbnail_405x552.webp",
-        price: "254",
-        discount: "36",
-        link: "",
-        sold: "12",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/05/13/165244123600952e503ccab89dd363c9b71f9094f8_thumbnail_405x552.webp",
-        price: "1687",
-        discount: "20",
-        link: "",
-        sold: "96",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/06/17/16554568735029a68267cacc48382e23027667093f_thumbnail_405x552.webp",
-        price: "126",
-        discount: "8",
-        link: "",
-        sold: "12",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/08/17/16607290552013caeedd00666350d71774fcd6e95f_thumbnail_405x552.webp",
-        price: "65",
-        discount: "7",
-        link: "",
-        sold: "82",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/09/02/166210785062f36a63bcecd00693783c4b6cd0c2a2_thumbnail_600x.webp",
-        price: "41",
-        discount: "9",
-        link: "",
-        sold: "52",
-    },
-    {
-        image: "https://img.ltwebstatic.com/images3_pi/2022/09/01/1662028598b9164c30a8410fc9d71b81fc63748d4e_thumbnail_405x552.webp",
-        price: "288",
-        discount: "35",
-        link: "",
-        sold: "49",
+        name: "",
+        image: "https://ae03.alicdn.com/kf/S882fd95abb7b40ac860d4a9fab1df995Y.jpg",
+        price: "",
     },
 ];
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,63 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
-import  { Header }  from '../components/header'
+import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 import { useSession, signIn, signOut } from "next-auth/react"
 import { Main } from '../components/home/main'
+import Category from '../components/home/category'
 import FlashDeals from '../components/home/flashDeals'
-
+import {
+  women_dresses,
+  women_shoes, women_accessories
+} from '../data/home'
+import { useMediaQuery } from "react-responsive";
 const inter = Inter({ subsets: ['latin'] })
+import ProductsSwiper from "../components/productsSwiper";
+import Product from "../models/Product";
+import ProductCard from "../components/productCard";
 
 export default function Home() {
   const { data: session } = useSession()
   console.log(session)
+  const isMedium = useMediaQuery({ query: "(max-width:850px)" });
+  const isMobile = useMediaQuery({ query: "(max-width:550px)" });
   return (
     <>
-    <div>
-      <Header/>
-      <div className={styles.home}>
-        <div className={styles.container}>
-          <Main />
-          <FlashDeals />
+      <div>
+        <Header />
+        <div className={styles.home}>
+          <div className={styles.container}>
+            <Main />
+            <FlashDeals />
+            <div className={styles.home__category}>
+              <Category header="Dres"
+                products={women_dresses}
+                background="#5a31f4" />
+                {
+                  !isMedium && 
+                  (
+                    <Category header="Sepatu"
+                    products={women_shoes}
+                    background="#f35b69" />
+                  )
+                }
+                 {
+                  isMobile && 
+                  (
+                    <Category header="Sepatu"
+                    products={women_shoes}
+                    background="#f35b69" />
+                  )
+                }
+              <Category header="Aksesoris"
+                products={women_accessories}
+                background="#000" />
+            </div>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
     </>
   )
 }
