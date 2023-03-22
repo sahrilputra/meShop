a58fe995310d340cd464a26cf1cd3f52573198c6
sahrilputra <iam.sahrilputra@gmail.com>
2023-03-23 06:50:49 +0700

    adding model product schema

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
-const { ObjectId } = mongoose.Schema;
+const {
+  ObjectId
+} = mongoose.Schema;
+
 const reviewSchema = new mongoose.Schema({
   reviewBy: {
     type: ObjectId,
@@ -9,7 +12,7 @@ const reviewSchema = new mongoose.Schema({
   rating: {
     type: Number,
     required: true,
-    default: 0,
+    default: 0
   },
   review: {
     type: String,
@@ -20,112 +23,93 @@ const reviewSchema = new mongoose.Schema({
   },
   style: {
     color: String,
-    image: String,
+    image: true,
   },
-  fit: {
+  rating: {
     type: String,
   },
   images: [],
   likes: [],
-});
-const productSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    brand: {
-      type: String,
-    },
-    slug: {
-      type: String,
-      required: true,
-      unique: true,
-      //lowercase: true,
-    },
-    category: {
-      type: ObjectId,
-      required: true,
-      ref: "Category",
-    },
-    subCategories: [
-      {
-        type: ObjectId,
-        ref: "subCategory",
-      },
-    ],
-    details: [
-      {
-        name: String,
-        value: String,
-      },
-    ],
-    questions: [
-      {
-        question: String,
-        answer: String,
-      },
-    ],
-    reviews: [reviewSchema],
-    refundPolicy: {
-      type: String,
-      default: "30 days",
-    },
-    rating: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    numReviews: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    shipping: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    subProducts: [
-      {
-        sku: String,
-        images: [],
-        description_images: [],
-        color: {
-          color: {
-            type: String,
-          },
-          image: {
-            type: String,
-          },
-        },
-        sizes: [
-          {
-            size: String,
-            qty: Number,
-            price: Number,
-          },
-        ],
-        discount: {
-          type: Number,
-          default: 0,
-        },
-        sold: {
-          type: Number,
-          default: 0,
-        },
-      },
-    ],
+})
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
-// const Product =
-//   mongoose.models.Product || mongoose.model("Product", productSchema);
+  description: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    ref: "Category",
+  },
+  subCategories: [{
+    type: ObjectId,
+    ref: 'subCategory'
+  }],
+  questions: [{
+    question: String,
+    answer: String,
+  }],
+  reviews: [reviewSchema],
+  refoundPolicy: {
+    type: String,
+    default: "30 days",
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  shipping: {
+    type: String,
+    required: true,
+    default: 0,
+  },
+  subProduct: [{
+    images: [],
+    description_images: [],
+    color: {
+      color: {
+        type: String,
+      },
+      image: {
+        type: String,
+      },
+    },
+    size: [{
+      size: String,
+      qty: Number,
+      price: Number,
+    }, ],
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+  }, ],
+}, {
+  timestamps: true,
+});
 
-// export default Product;
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
