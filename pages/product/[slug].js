d57e811339447ae852d5a2c4b985a83aed9fc1f4
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-03-27 20:49:04 +0700

    details colors menus

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,37 +4,63 @@ import Product from '../../models/Product'
 import Category from '../../models/Category'
 import SubCategory from '../../models/SubCategory'
 import Head from 'next/head';
-import { Header } from '../../components/header'
+import {
+    Header
+} from '../../components/header'
 import MainSwiper from '../../components/ProductPage/mainSwipper'
-import { useState } from 'react'
-import Infos from '../../components/ProductPage/info'
-export default function product({ product }) {
+import {
+    useState
+} from 'react'
+import {
+    Infos
+} from '../../components/ProductPage/info'
+export default function product({
+    product
+}) {
 
     const [activeImg, setActiveImg] = useState("");
 
     return (
         <>
-            <Head>
-                <title>{product.name}</title>
+            <Head >
+                <title >
+                    {
+                        product.name
+                    }
+                </title>
             </Head>
             <Header />
-            <div className={styles.product}>
-                <div className={styles.container}>
-                    <div className={styles.path}>
-                        Home / {product.category.name}
-                        {
+            <div className={styles.product} >
+                < div className={
+                    styles.container
+                } >
+                    <div className={
+                        styles.path
+                    } >
+                        Home / {
+                            product.category.name
+                        } {
                             product.subCategories.map((sub) => (
                                 <>
-                                    <span>/{sub.name}</span>
+                                    <span > /{sub.name}</span >
                                 </>
                             ))
-                        }
-                    </div>
+                        } </div>
 
-                    <div className={styles.product__main}>
-                        <MainSwiper images={product.images} activeImg={activeImg} />
-                        <Infos product={product} /> 
-                        <div></div>
+                    <div className={
+                        styles.product__main
+                    } >
+                        < MainSwiper images={
+                            product.images
+                        }
+                            activeImg={
+                                activeImg
+                            } />
+
+                        <Infos product={product} setActiveImg={setActiveImg} />
+                        <div >
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -44,15 +70,25 @@ export default function product({ product }) {
 
 
 export async function getServerSideProps(context) {
-    const { query } = context;
+    const {
+        query
+    } = context;
     const slug = query.slug;
     const style = query.style;
     const size = query.size || 0;
     db.connectDb();
     //============================
-    let product = await Product.findOne({ slug })
-        .populate({ path: "category", model: Category })
-        .populate({ path: "subCategories._id", model: SubCategory })
+    let product = await Product.findOne({
+        slug
+    })
+        .populate({
+            path: "category",
+            model: Category
+        })
+        .populate({
+            path: "subCategories._id",
+            model: SubCategory
+        })
         .lean();
     let subProduct = product.subProducts[style];
     let prices = subProduct.sizes.map((s) => {
@@ -70,14 +106,17 @@ export async function getServerSideProps(context) {
         colors: product.subProducts.map((p) => {
             return p.color;
         }),
-        priceRange: prices.length > 1 ?
-            `From ${prices[0]}$ to ${prices[prices.length - 1]}$`
-            : "",
-        price: subProduct.discount > 0
-            ? (subProduct.sizes[size].price -
+        priceRange: subProduct.discount ?
+            `From ${(prices[0] - prices[0] / subProduct.discount).toFixed(2)} to
+            ${(prices[prices.length - 1] -
+                prices[prices.length - 1] / subProduct.discount).toFixed(2)
+            }$` :
+            `From ${prices[0]}$ to ${prices[prices.length - 1]}$`,
+        price: subProduct.discount > 0 ?
+            (subProduct.sizes[size].price -
                 (subProduct.sizes[size].price / subProduct.discount)
-            ).toFixed(2)
-            : subProduct.sizes[size].price,
+            ).toFixed(2) :
+            subProduct.sizes[size].price,
         priceBefore: subProduct.sizes[size].price,
         quantity: subProduct.sizes[size].qty,
     }
