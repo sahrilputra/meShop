cae2233ff76845b3d9b730d1a828f514a8ec4b34
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-03-30 23:04:01 +0700

    fixing data slice cart

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,5 +1,6 @@
 import nc from 'next-connect'
 import db from '../../../utils/db'
+import Product from '../../../models/Product';
 const handler = nc();
 
 handler.get(async (req, res) => {
@@ -8,7 +9,26 @@ handler.get(async (req, res) => {
         const id = req.query.id;
         const style = req.query.style;
         const size = req.query.size;
-        console.log(id, style, size);
+        const product = await Product.findById(id).lean();
+        const discount = product.subProducts[style].discount;
+        const priceBefore = product.subProducts[style].sizes[size].price;
+        const price = discount ? priceBefore - priceBefore / discount : priceBefore;
+        db.disconnectDb();
+        return res.json({
+            _id: product._id,
+            style : Number(style),
+            name: product.name,
+            description : product.description,
+            slug: product.slug,
+            sku: product.subProducts[style].sku,
+            brand: product.brand,
+            shipping: product.brand,
+            images: product.subProducts[style].images,
+            color: product.subProducts[style].color,
+            price,
+            priceBefore,
+            quantity: product.subProducts[style].sizes[size].qty,
+        })
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
